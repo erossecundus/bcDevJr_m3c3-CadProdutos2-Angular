805b92b7532b71a9010278e34e47ba5665954b23
Eros Secundus <erossecundus@gmail.com>
2024-07-10 10:29:13 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'bootcamp-devjr-mod3cap3-CadProdutos2';
+}
